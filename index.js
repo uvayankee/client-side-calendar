b76f62c6b1ae39f339c67b6b43f5663d99dd3cd1
uvayankee <uvayankee@users.noreply.github.com>
2025-08-08 01:02:58 +0000

    deploy: ea014b42ab98ec13424793a9f818455f1b85399e

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+(() => {
+  // src/main.ts
+  console.log("Calendar app is running!");
+  var calendarContainer = document.getElementById("calendar-container");
+  if (calendarContainer) {
+    calendarContainer.textContent = "The calendar will be here.";
+  }
+})();
+//# sourceMappingURL=index.js.map
