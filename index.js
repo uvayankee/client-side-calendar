b1f151178c6b58b7f68f3be09381eec2d93aa4d6
uvayankee <uvayankee@users.noreply.github.com>
2025-08-09 01:53:10 +0000

    deploy: cc1d30ffb4764cc791e98f329e1d44d9cdebfbb7

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 "use strict";
 (() => {
   // src/calendar.ts
-  function renderCalendar(container, document2) {
+  function renderCalendar(container, document2, date = /* @__PURE__ */ new Date()) {
     if (container) {
+      container.innerHTML = "";
       const calendar = document2.createElement("div");
       calendar.classList.add("calendar");
-      const monthName = (/* @__PURE__ */ new Date()).toLocaleString("default", { month: "long" });
+      const header = document2.createElement("div");
+      header.classList.add("header");
+      const prevButton = document2.createElement("button");
+      prevButton.classList.add("prev-month");
+      prevButton.textContent = "< Prev";
+      prevButton.addEventListener("click", () => {
+        const newDate = new Date(date);
+        newDate.setMonth(newDate.getMonth() - 1);
+        renderCalendar(container, document2, newDate);
+      });
+      header.appendChild(prevButton);
       const monthNameElement = document2.createElement("div");
       monthNameElement.classList.add("month-name");
-      monthNameElement.textContent = monthName;
-      calendar.appendChild(monthNameElement);
+      monthNameElement.textContent = date.toLocaleString("default", { month: "long", year: "numeric" });
+      header.appendChild(monthNameElement);
+      const nextButton = document2.createElement("button");
+      nextButton.classList.add("next-month");
+      nextButton.textContent = "Next >";
+      nextButton.addEventListener("click", () => {
+        const newDate = new Date(date);
+        newDate.setMonth(newDate.getMonth() + 1);
+        renderCalendar(container, document2, newDate);
+      });
+      header.appendChild(nextButton);
+      calendar.appendChild(header);
       container.appendChild(calendar);
     }
   }
